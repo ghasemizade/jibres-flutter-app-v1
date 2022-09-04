1bebbbeac6894a4c28b7e81c8429ddf6339d53e2
Hosein <hosein@ghasemizade.com>
2022-09-04 21:11:56 +0430

    update customers intro screen

--- lib/Intro/IntroEnglish_customer.dart
+++ lib/Intro/IntroEnglish_customer.dart
@@ -11,75 +11,197 @@ class IntroEnglishCustomer extends StatefulWidget {
 }
 
 class _IntroEnglishCustomerState extends State<IntroEnglishCustomer> {
+  late PageController _pageController;
+  List<Widget> slideList = [];
+  int initialPage = 3;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: HexColor(from1),
-      body: SafeArea(
-        child: Center(
-          child: Stack(
-            //mainAxisAlignment: MainAxisAlignment.end,
+        backgroundColor: HexColor(from1),
+        body: SafeArea(
+          child: Column(
             children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(40),
-                        topRight: Radius.circular(40),
-                      ),
-                    ),
-                    height: 280.0,
-                    child: Center(
-                      child: Column(
-                        children: [
-                          SizedBox(
-                            height: 120.0,
-                          ),
-                          Text(
-                            title1,
-                            style: TextStyle(fontSize: 28.0),
-                          ),
-                          SizedBox(
-                            height: 15.0,
-                          ),
-                          Text(
-                            desc1,
-                            style: TextStyle(fontSize: 20.0),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              Column(
-                children: [
-                  SizedBox(
-                    height: 50.0,
-                  ),
-                  Center(
-                    child: SizedBox(
-                      height: 450.0,
-                      width: 300.0,
-                      child: Card(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20.0),
-                        ),
-                        child: Image(
-                          image: AssetImage('Images/Jibres-Logo-icon-512.png'),
-                          width: 100.0,
-                        ),
-                        elevation: 30,
-                      ),
-                    ),
-                  ),
-                ],
+              Expanded(
+                child: PageView.builder(
+                  itemBuilder: ((context, index) => IntroEnglishCustomerContent(
+                        image: 'Images/Jibres-Logo-icon-512.png',
+                        titles: title1,
+                        description: desc1,
+                      )),
+                ),
               ),
             ],
           ),
+        ));
+  }
+}
+
+class IntroEnglishCustomerContent extends StatelessWidget {
+  const IntroEnglishCustomerContent(
+      {Key? key,
+      required this.image,
+      required this.titles,
+      required this.description})
+      : super(key: key);
+
+  final String image, titles, description;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Center(
+        child: Stack(
+          //mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Column(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(40),
+                          topRight: Radius.circular(40),
+                        ),
+                      ),
+                      height: 280.0,
+                      child: Center(
+                        child: Column(
+                          children: [
+                            SizedBox(
+                              height: 120.0,
+                            ),
+                            Text(
+                              titles,
+                              style: TextStyle(fontSize: 28.0),
+                            ),
+                            SizedBox(
+                              height: 15.0,
+                            ),
+                            Text(
+                              description,
+                              style: TextStyle(fontSize: 20.0),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                SizedBox(
+                  height: 50.0,
+                ),
+                Center(
+                  child: SizedBox(
+                    height: 450.0,
+                    width: 300.0,
+                    child: Column(
+                      children: [
+                        Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                          child: Image(
+                            image: AssetImage(image),
+                            width: 100.0,
+                          ),
+                          elevation: 30,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Column(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(40),
+                          topRight: Radius.circular(40),
+                        ),
+                      ),
+                      height: 280.0,
+                      child: Center(
+                        child: Column(
+                          children: [
+                            SizedBox(
+                              height: 100.0,
+                            ),
+                            Text(
+                              title1,
+                              style: TextStyle(fontSize: 28.0),
+                            ),
+                            SizedBox(
+                              height: 15.0,
+                            ),
+                            Text(
+                              desc1,
+                              style: TextStyle(fontSize: 20.0),
+                            ),
+                            SizedBox(
+                              height: 20.0,
+                            ),
+                            SizedBox(
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  ElevatedButton(
+                                    onPressed: () {},
+                                    child: Icon(
+                                        Icons.keyboard_arrow_right_outlined),
+                                    style: ElevatedButton.styleFrom(
+                                      primary: HexColor(from1),
+                                      shape: CircleBorder(),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                SizedBox(
+                  height: 20.0,
+                ),
+                Center(
+                  child: SizedBox(
+                    height: 450.0,
+                    width: 300.0,
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                      child: Image(
+                        image: AssetImage(image),
+                        width: 100.0,
+                      ),
+                      elevation: 35,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
